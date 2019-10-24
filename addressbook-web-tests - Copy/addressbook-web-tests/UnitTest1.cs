db967739f8415011eb723adb305489232200169f
Maxim-1975 <mkolpakov@gmail.com>
2019-10-24 20:02:32 +0300

    Change 20191024

---
+++ addressbook-web-tests - Copy/addressbook-web-tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace addressbook_web_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+//test
+//test1
+        }
+    }
+}
